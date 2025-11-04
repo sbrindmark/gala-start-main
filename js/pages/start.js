3abedfd8234d2bd43bb7369b57da775b0e689348
sbrindmark <sebastianbrindmark@gmail.com>
2025-11-04 11:03:37 +0100

    La in css som Tsoler skapat

--- js/pages/start.js
+++ js/pages/start.js
@@ -3,8 +3,19 @@ import clubInfoAndEvents from "../utils/club-info-and-events.js";
 export default async function start() {
   const eventHtml = await clubInfoAndEvents();
   return `
-    <h1>Alla kommande events på Gala</h1>
-    <p>Gala är en samlingsplats för olika musikklubbar.</p>
-    ${eventHtml}
+    <section class="clubs">
+      <h2>Våra Klubbar</h2>
+      <div class="club-grid">
+        <a href="#jazz-klubben" class="club jazz">Jazz Club</a>
+        <a href="#metal-klubben" class="club dance">Dance Club</a>
+        <a href="#comedy-klubben" class="club comedy">Stand-Up Comedy</a>
+        <a href="#theatre-klubben" class="club theatre">Theatre Nights</a>
+      </div>
+      <div id="all-events-container">
+        <h2>Alla kommande events på Gala</h2>
+        <p>Gala är en samlingsplats för olika musikklubbar.</p>
+        ${eventHtml}
+      </div>
+    </section>
   `;
 }
