d58d037259c6956328182774e0938ec0f5629d45
sbrindmark <sebastianbrindmark@gmail.com>
2025-11-05 14:05:42 +0100

    Revert "Added json clubs och evenement och created html"
    This reverts commit b883e347de2b941882fdb2c2c096c0fad36af156.

--- js/pages/start.js
+++ js/pages/start.js
@@ -9,7 +9,7 @@ export default async function start() {
         <a href="#jazz-klubben" class="club jazz">Jazz Club</a>
         <a href="#metal-klubben" class="club metal">Metal Club</a>
         <a href="#comedy-klubben" class="club comedy">Stand-Up Comedy</a>
-        <a href="#house-technoklubben" class="club house-techno">Theatre Nights</a>
+        <a href="#theatre-klubben" class="club theatre">Theatre Nights</a>
       </div>
       <div id="all-events-container">
         <h2>Alla kommande events på Gala</h2>
